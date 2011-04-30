1740fdd88ae624379bc600b3c6948e40363ea0bc
Emmanuel Delgado <emmanuel.delgado@crowdint.com>
2011-04-30 14:21:12 -0500

    * Authentication works now, using foursquare gem

--- app/routes/foodfinder.rb
+++ app/routes/foodfinder.rb
@@ -1,20 +1,27 @@
 class FoodFinder < Sinatra::Base
   configure do
     set :haml, { :format => :html5 }
-    set :views, 'app/views'
+  end
+
+  def client
+    oauth = Foursquare::OAuth.new('M3Z2OBWQB4RPYNY22S112I4DVI5W4VQB5LRDJRPGEK1LK5RO', '53BWA4J3YLU4ZEM2NNNAHFQMG0AOAZ3GRO25UBRD5OFAWGLC')
+    oauth.set_callback_url redirect_uri
+    oauth
   end
 
   get '/auth' do
-    redirect '/auth/foursquare'
+    redirect client.request_token.authorize_url
   end
 
-  get '/auth/foursquare/callback' do
-    auth_hash = request.env['omniauth.auth']
-    auth_hash.inspec
+  get '/auth/callback' do
+    token, verifier = params['oauth_token'], params['oauth_verifier']
   end
 
-  get '/auth/failure' do
-    params.inspect
+  def redirect_uri
+    uri = URI.parse(request.url)
+    uri.path = '/auth/callback'
+    uri.query = nil
+    uri.to_s
   end
 
 end
