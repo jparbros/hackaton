17419970f6247ed5221981c549d6e8c280e75c88
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-04-30 12:28:58 -0500

    changes

--- lib/setup.rb
+++ lib/setup.rb
@@ -1,6 +1,4 @@
 class Setup  
-  class ActiveRecordConfigFile < Exception ; end
-
   class << self 
     def start
       check_requirements
@@ -21,18 +19,10 @@ class Setup
 
 
     def initialize_connection_mongodb
-      mongoid_conf = storage_config
-      Mongoid.configure do |config|
-        config.master = Mongo::Connection.from_uri("mongodb://#{mongoid_conf['host']}:#{mongoid_conf['port']}").db(mongoid_conf['database'])
+      if ENV['MONGOHQ_URL']
+        config.master = Mongo::Connection.from_uri("mongodb://#{ENV['MONGOHQ_URL']}")      
       end
     end
 
-    def storage_config
-      @config ||= YAML.load_file(File.dirname(__FILE__) + '/../config/mongodb.yml')[ENV['SINATRA_ENV']]
-    end
-
-    def check_requirements
-      raise ActiveRecordConfigFile.new('Config file not found') unless Dir.getwd + '/../config/mongodb.yml'
-    end
   end
 end
