c91698f80854154ded0abf4036dccdd2bbfb8186
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-04-30 13:16:26 -0500

    Views configured and layout

--- app/routes/foodfinder.rb
+++ app/routes/foodfinder.rb
@@ -1,9 +1,11 @@
 class FoodFinder < Sinatra::Base
   configure do
     set :haml, { :format => :html5 }
+    set :views, 'app/views'
   end
 
   get '/' do
     puts "here"
+    haml :index
   end
 end
