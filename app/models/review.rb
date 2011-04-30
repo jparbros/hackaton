8a7bd2de2a653732e59dcd220c21484bab09fe66
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-04-30 15:24:24 -0500

    changed the hash declaration

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review
   include Mongoid::Document
   
-  field :type, type: String
-  field :rating, type: Integer, default: 0
-  field :venue_id, type: Integer
+  field :type, :type => String
+  field :rating, :type => Integer, :default => 0
+  field :venue_id, :type => Integer
 end
