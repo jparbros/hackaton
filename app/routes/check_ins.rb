ee5d5544a15ba69392aadb15acf18d51f0420409
Emmanuel Delgado <emmanuel.delgado@crowdint.com>
2011-05-02 11:10:55 -0500

    * Fix typo inside haml view

--- app/routes/check_ins.rb
+++ app/routes/check_ins.rb
@@ -9,8 +9,8 @@ class FoodFinder < Sinatra::Base
 
   post '/checkins' do
      response = Client.check_in params[:venue_id]
-     puts response.inspect
-     CheckIn.create :checkin_id => response['checkin']['id'], :user_id => Session[:user]['id']
+     CheckIn.create :checkin_id => response['checkin']['id'], 
+                    :user_id => session[:foodfinder]['user']['response']['user']['id']
      redirect '/'
   end
 end
