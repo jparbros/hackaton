ee71afaf75f42e021aaeed73eb5d5acbc2295e7c
Emmanuel Delgado <emmanuel.delgado@crowdint.com>
2011-05-05 16:29:39 -0500

    * Creating dev configurations

--- lib/config/dev.rb
+++ lib/config/dev.rb
@@ -9,5 +9,22 @@ module FoodFinder::Config
     def database_name
       "foodfinder"
     end
+
+    def auth_url
+      'https://foursquare.com/oauth2'
+    end
+
+    def api_url
+      "https://api.foursquare.com/v2"
+    end
+
+    def client_id
+      'M3Z2OBWQB4RPYNY22S112I4DVI5W4VQB5LRDJRPGEK1LK5RO'
+    end
+
+    def secret
+      '53BWA4J3YLU4ZEM2NNNAHFQMG0AOAZ3GRO25UBRD5OFAWGLC'
+    end
+
   end
 end
